465de11cde5aacbad5591de9f360bd0c00f36379
Dean Donkov <nezpower@abv.bg>
2022-12-13 13:21:41 +0200

    Add more environment variables, reduce files and optimize volumes

--- Polygon/polygon.sh
+++ Polygon/polygon.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-mkdir /root/logs \
+mkdir -p /root/logs \
     && touch /root/logs/heimdalld.log \
     && touch /root/logs/rest-server.log \
     && touch /root/logs/bridge.log \
     && touch /root/logs/bor.log
-# heimdall configuration
-sed -i "/seeds =/c\seeds = \"${HEIMDALL_SEEDS}\"" ${HEIMDALL_HOME}/config/config.toml \
-    && sed -i "/26657/c\laddr = \"${HEIMDALL_BRIDGE}\"" ${HEIMDALL_HOME}/config/config.toml \
-    && sed -i "/bor_rpc_url/c\bor_rpc_url = \"${HEIMDALL_BOR_RPC_URL}\"" ${HEIMDALL_HOME}/config/config.toml \
-    && sed -i "/eth_rpc_url/c\eth_rpc_url = \"${HEIMDALL_ETH_RPC_URL}\"" ${HEIMDALL_HOME}/config/config.toml
 #start heimdall, heimdall rest server with logs.
-echo $HTTP_PORT
-echo $ADDRESS
-WS_PORT=2222
-echo $WS_PORT
-
 heimdalld start 2>&1 | tee /root/logs/heimdalld.log &
     heimdalld rest-server 2>&1 | tee /root/logs/rest-server.log &
     bridge start 2>&1 | tee /root/logs/bridge.log &
