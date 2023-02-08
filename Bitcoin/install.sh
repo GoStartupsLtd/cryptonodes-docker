4d3b07f09c91fa561f2cb7c1be07d9ebbb3428a2
Dean Donkov <nezpower@abv.bg>
2023-02-08 11:54:54 +0200

    Add apt update, fix single quotes and add echo to show the version we are downloading

--- Bitcoin/install.sh
+++ Bitcoin/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Installing version: $BITCOIN_VERSION"
+
 wget "https://bitcoincore.org/bin/bitcoin-core-$BITCOIN_VERSION/bitcoin-$BITCOIN_VERSION-x86_64-linux-gnu.tar.gz" \
     && tar xzf bitcoin-$BITCOIN_VERSION-x86_64-linux-gnu.tar.gz \
     && install -m 0755 -o root -g root -t /usr/local/bin bitcoin-$BITCOIN_VERSION/bin/*
