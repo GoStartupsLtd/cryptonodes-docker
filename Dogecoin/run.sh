4d0c3498c8d77930bb67f657d131c2d8555158ec
Dean Donkov <nezpower@abv.bg>
2023-02-09 23:44:44 +0200

    Change port to 22554 as 22555 is an official port from IANA and conflicts with other services and set port through -port instead of -rpcport as it does not work with -rpcport

--- Dogecoin/run.sh
+++ Dogecoin/run.sh
@@ -2,14 +2,14 @@
 
 RPC_USER="${RPC_USER:-}"
 RPC_PASS="${RPC_PASS:-}"
-RPC_PORT="${RPC_PORT:-22555}"
+RPC_PORT="${RPC_PORT:-22554}"
 RPC_ALLOW_IP="${RPC_ALLOW_IP:-127.0.0.1}"
 RPC_BIND="${RPC_BIND:-0.0.0.0}"
 TESTNET="${RPC_TESTNET:-0}"
 EXTRAFLAGS="${EXTRAFLAGS:-}"
 DEBIAN_FRONTEND="${DEBIAN_FRONTEND:-noninteractive}"
 
-exec dogecoind -printtoconsole \
+exec dogecoind -port=$RPC_PORT \
     -rpcuser=$RPC_USER \
     -rpcpassword=$RPC_PASS \
     -rpcport=$RPC_PORT \
@@ -18,4 +18,5 @@ exec dogecoind -printtoconsole \
     -listen \
     -server \
     -testnet=$TESTNET \
+    -printtoconsole \
     $EXTRAFLAGS
