56de3f5e0f7d07a3b42decdc759918a5c1b9ea46
Dean Donkov <nezpower@abv.bg>
2023-02-09 16:25:48 +0200

    Fetch dogecoin from latest version

---
+++ Dogecoin/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+RPC_USER="${RPC_USER:-}"
+RPC_PASS="${RPC_PASS:-}"
+RPC_PORT="${RPC_PORT:-22555}"
+RPC_ALLOW_IP="${RPC_ALLOW_IP:-127.0.0.1}"
+RPC_BIND="${RPC_BIND:-0.0.0.0}"
+TESTNET="${RPC_TESTNET:-0}"
+EXTRAFLAGS="${EXTRAFLAGS:-}"
+DEBIAN_FRONTEND="${DEBIAN_FRONTEND:-noninteractive}"
+
+exec dogecoind -printtoconsole \
+    -rpcuser=$RPC_USER \
+    -rpcpassword=$RPC_PASS \
+    -rpcport=$RPC_PORT \
+    -rpcallowip=$RPC_ALLOW_IP \
+    -rpcbind=$RPC_BIND \
+    -listen \
+    -server \
+    -testnet=$TESTNET \
+    $EXTRAFLAGS
