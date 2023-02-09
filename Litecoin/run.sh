dea5486ffb43c270a3d33a5b703585cabe24e3d4
Dean Donkov <nezpower@abv.bg>
2023-02-09 23:31:50 +0200

    Pass port as --port instead of --rpcport as it does not work

--- Litecoin/run.sh
+++ Litecoin/run.sh
@@ -2,14 +2,14 @@
 
 RPC_USER="${RPC_USER:-}"
 RPC_PASS="${RPC_PASS:-}"
-RPC_PORT="${RPC_PORT:-22557}"
+RPC_PORT="${RPC_PORT:-22556}"
 RPC_ALLOW_IP="${RPC_ALLOW_IP:-127.0.0.1}"
 RPC_BIND="${RPC_BIND:-0.0.0.0}"
 TESTNET="${RPC_TESTNET:-0}"
 EXTRAFLAGS="${EXTRAFLAGS:-}"
 DEBIAN_FRONTEND="${DEBIAN_FRONTEND:-noninteractive}"
 
-exec litecoind -rpcuser=$RPC_USER \
+exec litecoind -port=$RPC_PORT \
     -rpcpassword=$RPC_PASS \
     -rpcport=$RPC_PORT \
     -rpcallowip=$RPC_ALLOW_IP \
