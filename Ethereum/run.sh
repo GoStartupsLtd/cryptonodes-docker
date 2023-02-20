de761a672fc962e068f18a8848a0bef38cc534ed
Dean Donkov <nezpower@abv.bg>
2023-02-20 11:47:17 +0200

    Fix prysm script not being executed and separate arguments

--- Ethereum/run.sh
+++ Ethereum/run.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+
 HTTP_PORT="${HTTP_PORT:-8545}"
 WS_PORT="${WS_PORT:-8546}"
 PORT="${PORT:-30303}"
 SYNCMODE="${SYNCMODE:-snap}"
-EXTRAFLAGS="${EXTRAFLAGS:-}"
-ADDRESS="${ADDRESS:-127.0.0.1}"
+EXTRAFLAGS="${EXTRAFLAGS}"
 
-exec geth --syncmode ${SYNCMODE} --nat none --txlookuplimit 0 --cache 4096 --port ${PORT} \
---http --http.addr 0.0.0.0 --http.port=${HTTP_PORT} --http.vhosts '*' --http.corsdomain '*' \
---ws --ws.addr ${ADDRESS} --ws.port ${WS_PORT} --ws.origins '*' ${EXTRAFLAGS} | \
-./prysm.sh beacon-chain --execution-endpoint=/root/.ethereum/geth.ipc --accept-terms-of-use
+exec geth --syncmode ${SYNCMODE} \
+    --nat none \
+    --txlookuplimit 0 \
+    --cache 4096
+    --port ${PORT} \
+    --http \
+    --http.addr 0.0.0.0 \
+    --http.port=${HTTP_PORT} \
+    --http.vhosts '*' \
+    --http.corsdomain '*' \
+    --ws \
+    --ws.addr ${ADDRESS} \
+    --ws.port ${WS_PORT} \
+    --ws.origins '*' \
+    ${EXTRAFLAGS} & ./prysm.sh beacon-chain --execution-endpoint=/root/.ethereum/geth.ipc --accept-terms-of-use &
+ 
