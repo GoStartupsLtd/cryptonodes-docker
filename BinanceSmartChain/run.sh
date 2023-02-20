c4cacc66a6f292a3a2ea15bb0d177b0cd90593a5
Dean Donkov <nezpower@abv.bg>
2023-02-20 11:18:08 +0200

    Make parameters separate on each line, and make a multi stage build

--- BinanceSmartChain/run.sh
+++ BinanceSmartChain/run.sh
@@ -7,7 +7,22 @@ PORT="${PORT:-30311}"
 SYNCMODE="${SYNCMODE:-snap}"
 EXTRAFLAGS="${EXTRAFLAGS:-""}"
 
-exec ./geth_linux --ipcdisable --syncmode ${SYNCMODE} --nat none --rpc.allow-unprotected-txs --txlookuplimit 0 \
-    --cache 4096 --port ${PORT} --http --http.addr ${ADDRESS} --http.port=${HTTP_PORT} --http.vhosts '*' \
-    --http.corsdomain '*' --ws --ws.addr ${ADDRESS} --ws.port ${WS_PORT} --ws.origins '*' --config ./config.toml \
-    ${EXTRAFLAGS} \
+exec geth_linux \
+    --ipcdisable \
+    --syncmode ${SYNCMODE} \
+    --nat none \
+    --rpc.allow-unprotected-txs \
+    --txlookuplimit 0 \
+    --cache 4096 \
+    --port ${PORT} \
+    --http \
+    --http.addr ${ADDRESS} \
+    --http.port=${HTTP_PORT} \
+    --http.vhosts '*' \
+    --http.corsdomain '*' \
+    --ws \
+    --ws.addr ${ADDRESS} \
+    --ws.port ${WS_PORT} \
+    --ws.origins '*' \
+    --config ./config.toml \
+    ${EXTRAFLAGS}
