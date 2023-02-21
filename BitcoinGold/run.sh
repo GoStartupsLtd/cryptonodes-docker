c9da405197ea197318a392c011178c71ab526d49
Dean Donkov <nezpower@abv.bg>
2023-02-21 15:20:24 +0200

    Make bitcoin gold build latest release (#16)
    * Make bitcoin gold build latest release
    
    * Make bitcoin gold multi-stage build
    
    * Make RPC port 8333 due to Bitcoin being on 8332
    
    * Add RPC user and password to bgoldd parameters
    
    * Add exec
    
    * Remove unused exported port

---
+++ BitcoinGold/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+RPC_USER="${RPC_USER:-user}"
+RPC_PASS="${RPC_PASS:-pass}"
+RPC_PORT="${RPC_PORT:-8333}"
+RPC_ALLOW_IP="${RPC_ALLOW_IP:-127.0.0.1}"
+TESTNET="${TESTNET:-0}"
+EXTRAFLAGS="${EXTRAFLAGS:-""}"
+DEBIAN_FRONTEND="${DEBIAN_FRONTEND:-noninteractive}"
+RPC_BIND="${RPC_BIND:-0.0.0.0}"
+
+exec bgoldd -port=$RPC_PORT \
+    -rpcuser=$RPC_USER \
+    -rpcpassword=$RPC_PASS \
+    -rpcallowip=$RPC_ALLOW_IP \
+    -rpcbind=$RPC_BIND \
+    -listen \
+    -server \
+    -testnet=$TESTNET \
+    $EXTRAFLAGS
