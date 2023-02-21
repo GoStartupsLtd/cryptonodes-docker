3f0fae98407f02c6063e55827a4ccbe37fc411cb
Dean Donkov <nezpower@abv.bg>
2023-02-21 15:21:14 +0200

    Fetch dogecoin from latest version (#12)
    * Fetch dogecoin from latest version
    
    * Change port to 22554 as 22555 is an official port from IANA and conflicts with other services and set port through -port instead of -rpcport as it does not work with -rpcport
    
    * Remove useless AS build
    
    * Make dogecoin multi-stage build

---
+++ Dogecoin/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+RPC_USER="${RPC_USER:-}"
+RPC_PASS="${RPC_PASS:-}"
+RPC_PORT="${RPC_PORT:-22554}"
+RPC_ALLOW_IP="${RPC_ALLOW_IP:-127.0.0.1}"
+RPC_BIND="${RPC_BIND:-0.0.0.0}"
+TESTNET="${RPC_TESTNET:-0}"
+EXTRAFLAGS="${EXTRAFLAGS:-}"
+DEBIAN_FRONTEND="${DEBIAN_FRONTEND:-noninteractive}"
+
+exec dogecoind -port=$RPC_PORT \
+    -rpcuser=$RPC_USER \
+    -rpcpassword=$RPC_PASS \
+    -rpcallowip=$RPC_ALLOW_IP \
+    -rpcbind=$RPC_BIND \
+    -listen \
+    -server \
+    -testnet=$TESTNET \
+    -printtoconsole \
+    $EXTRAFLAGS
