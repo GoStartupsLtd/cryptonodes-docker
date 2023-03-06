2e1e009a934ac29bf2cc5b3f11aabe20d5dc4b1d
Dean Donkov <nezpower@abv.bg>
2023-03-06 14:31:34 +0200

    Update Dockerfile

--- Okt/run.sh
+++ Okt/run.sh
@@ -11,19 +11,12 @@ EXTRAFLAGS="${EXTRAFLAGS:-}"
 START_FROM_SNAPSHOT="${START_FROM_SNAPSHOT:-true}"
 
 if [ "${START_FROM_SNAPSHOT}" = "true" ]; then
-    wget https://okg-pub-hk.oss-cn-hongkong.aliyuncs.com/cdn/okc/snapshot/mainnet-s1-20230306-17837049-rocksdb.tar.gz && \
-    tar -zxvf mainnet-s1-20230306-17837049-rocksdb.tar.gz -C ${EXCHAIND_HOME} && \
-    exec ./exchaind start \
-    --home ${EXCHAIND_HOME} \
-    --rpc.laddr tcp://${ADDRESS}:${TENDERMINT_RPC_PORT} \
-    --rest.laddr tcp://${ADDRESS}:${EVM_RPC_PORT} \
-    --wsport=${WS_PORT} \
-    --iavl-enable-async-commit=false \
-    --fast-query=false \
-    ${EXTRAFLAGS}
+    wget -q -O - https://okg-pub-hk.oss-cn-hongkong.aliyuncs.com/cdn/okc/snapshot/mainnet-s1-20230306-17837049-rocksdb.tar.gz | tar -zxv -C ${EXCHAIND_HOME}
 else
-    curl --create-dirs https://raw.githubusercontent.com/okx/mainnet/main/genesis.json -o ${EXCHAIND_HOME}/config/genesis.json && \
-    exec ./exchaind start \
+    curl --create-dirs https://raw.githubusercontent.com/okx/mainnet/main/genesis.json -o ${EXCHAIND_HOME}/config/genesis.json
+fi
+
+exec ./exchaind start \
     --home ${EXCHAIND_HOME} \
     --rpc.laddr tcp://${ADDRESS}:${TENDERMINT_RPC_PORT} \
     --rest.laddr tcp://${ADDRESS}:${EVM_RPC_PORT} \
@@ -31,4 +24,3 @@ else
     --iavl-enable-async-commit=false \
     --fast-query=false \
     ${EXTRAFLAGS}
-fi
