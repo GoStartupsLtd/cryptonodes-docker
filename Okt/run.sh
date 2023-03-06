0b619771787efc1b843db1620d530f5ee041dc1e
Dean Donkov <nezpower@abv.bg>
2023-03-06 12:04:21 +0200

    Add okt node

---
+++ Okt/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ADDRESS="${ADDRESS:-0.0.0.0}"
+EVM_RPC_PORT="${HTTP_PORT:-8549}"
+TENDERMINT_RPC_PORT="${HTTP_PORT:-8550}"
+WS_PORT="${WS_PORT:-8551}"
+EXCHAIND_HOME="${EXCHAIND_HOME:-/home/exchaind}"
+CHAIN_ID="${CHAIN_ID:-exchain-66}"
+EXTRAFLAGS="${EXTRAFLAGS:-}"
+
+curl --create-dirs https://raw.githubusercontent.com/okx/mainnet/main/genesis.json -o ${EXCHAIND_HOME}/config/genesis.json && \
+    exec exchaind start \
+    --home ${EXCHAIND_HOME} \
+    --rpc.laddr tcp://${ADDRESS}:${TENDERMINT_RPC_PORT} \
+    --rest.laddr tcp://${ADDRESS}:${EVM_RPC_PORT} \
+    --wsport=${WS_PORT} \
+    --iavl-enable-async-commit=false \
+    --fast-query=false \
+    ${EXTRAFLAGS}
