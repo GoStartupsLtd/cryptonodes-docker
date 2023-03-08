a222635fb35bd9407f94c4a1a397e2c09fc983cd
Dean Donkov <nezpower@abv.bg>
2023-03-08 22:56:44 +0200

    Fix bitcoin gold

--- BitcoinGold/run.sh
+++ BitcoinGold/run.sh
@@ -2,18 +2,23 @@
 
 RPC_USER="${RPC_USER:-user}"
 RPC_PASS="${RPC_PASS:-pass}"
-RPC_PORT="${RPC_PORT:-8333}"
-RPC_ALLOW_IP="${RPC_ALLOW_IP:-127.0.0.1}"
+
+RPC_PORT="${RPC_PORT:-8336}"
+RPC_ALLOW_IP="${RPC_ALLOW_IP:-0.0.0.0/0}"
 TESTNET="${TESTNET:-0}"
 EXTRAFLAGS="${EXTRAFLAGS:-""}"
 DEBIAN_FRONTEND="${DEBIAN_FRONTEND:-noninteractive}"
 RPC_BIND="${RPC_BIND:-0.0.0.0}"
+DATA_DIR="${DATA_DIR:-/home/bgoldd}"
 
-exec bgoldd -port=$RPC_PORT \
-    -rpcuser=$RPC_USER \
-    -rpcpassword=$RPC_PASS \
+mkdir -p ${DATA_DIR} &&
+    exec bgoldd -rpcport=$RPC_PORT \
+    -datadir=$DATA_DIR \
     -rpcallowip=$RPC_ALLOW_IP \
     -rpcbind=$RPC_BIND \
+    -rpcuser=$RPC_USER \
+    -rpcpassword=$RPC_PASS \
+    -rest \
     -listen \
     -server \
     -testnet=$TESTNET \
