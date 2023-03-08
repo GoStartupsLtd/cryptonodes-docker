bac036e732b1b679e24ad72f5c24a945acd3d596
Dean Donkov <nezpower@abv.bg>
2023-03-08 23:13:24 +0200

    Better variable placing

--- BitcoinGold/run.sh
+++ BitcoinGold/run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+DATA_DIR="${DATA_DIR:-/home/bgoldd}"
+
+
 RPC_USER="${RPC_USER:-user}"
 RPC_PASS="${RPC_PASS:-pass}"
-
 RPC_PORT="${RPC_PORT:-8336}"
 RPC_ALLOW_IP="${RPC_ALLOW_IP:-0.0.0.0/0}"
-TESTNET="${TESTNET:-0}"
-EXTRAFLAGS="${EXTRAFLAGS:-""}"
-DEBIAN_FRONTEND="${DEBIAN_FRONTEND:-noninteractive}"
 RPC_BIND="${RPC_BIND:-0.0.0.0}"
-DATA_DIR="${DATA_DIR:-/home/bgoldd}"
+
+TESTNET="${TESTNET:-0}"
+DEBIAN_FRONTEND="${DEBIAN_FRONTEND:-noninteractive}"
+EXTRAFLAGS="${EXTRAFLAGS:-""}"
 
 mkdir -p ${DATA_DIR} && 
     exec bgoldd -rpcport=$RPC_PORT \
