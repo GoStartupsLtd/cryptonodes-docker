3294bf594a1c8d057cc4e4b235d8f4456978a9fa
Dean Donkov <nezpower@abv.bg>
2023-03-09 11:38:59 +0200

    Fix bitcoin gold, bitcoin cash, litecoin nodes RPC (#27)
    * Start bitcoin cash as server instead of client to fix RPC authentication and fix rpc port
    
    * Remove data dir
    
    * Remove mkdir
    
    * Change bitcoin cash RPC port to default
    
    * Fix bitcoin gold
    
    * Start as server instead of client
    
    * Better variable placing
    
    * Remove empty line
    
    * Remove datadir
    
    * Remove mkdir
    
    * Change rpc port to default for bitcoin gold
    
    * Fix litecoin port
    
    * Start as server not as client, as RPC username and password dont work unless started as server
    
    * Remove data dir

--- BitcoinGold/run.sh
+++ BitcoinGold/run.sh
@@ -2,19 +2,20 @@
 
 RPC_USER="${RPC_USER:-user}"
 RPC_PASS="${RPC_PASS:-pass}"
-RPC_PORT="${RPC_PORT:-8333}"
-RPC_ALLOW_IP="${RPC_ALLOW_IP:-127.0.0.1}"
-TESTNET="${TESTNET:-0}"
-EXTRAFLAGS="${EXTRAFLAGS:-""}"
-DEBIAN_FRONTEND="${DEBIAN_FRONTEND:-noninteractive}"
+RPC_PORT="${RPC_PORT:-8332}"
+RPC_ALLOW_IP="${RPC_ALLOW_IP:-0.0.0.0/0}"
 RPC_BIND="${RPC_BIND:-0.0.0.0}"
 
-exec bgoldd -port=$RPC_PORT \
-    -rpcuser=$RPC_USER \
-    -rpcpassword=$RPC_PASS \
+TESTNET="${TESTNET:-0}"
+DEBIAN_FRONTEND="${DEBIAN_FRONTEND:-noninteractive}"
+EXTRAFLAGS="${EXTRAFLAGS:-""}"
+
+exec bgoldd -rpcport=$RPC_PORT \
     -rpcallowip=$RPC_ALLOW_IP \
     -rpcbind=$RPC_BIND \
-    -listen \
+    -rpcuser=$RPC_USER \
+    -rpcpassword=$RPC_PASS \
+    -rest \
     -server \
     -testnet=$TESTNET \
     $EXTRAFLAGS
--- Litecoin/run.sh
+++ Litecoin/run.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-RPC_USER="${RPC_USER:-}"
-RPC_PASS="${RPC_PASS:-}"
+RPC_USER="${RPC_USER:-user}"
+RPC_PASS="${RPC_PASS:-pass}"
 RPC_PORT="${RPC_PORT:-22556}"
-RPC_ALLOW_IP="${RPC_ALLOW_IP:-127.0.0.1}"
+RPC_ALLOW_IP="${RPC_ALLOW_IP:-0.0.0.0/0}"
 RPC_BIND="${RPC_BIND:-0.0.0.0}"
-TESTNET="${RPC_TESTNET:-0}"
-EXTRAFLAGS="${EXTRAFLAGS:-}"
-DEBIAN_FRONTEND="${DEBIAN_FRONTEND:-noninteractive}"
 
-exec litecoind -port=$RPC_PORT \
-    -rpcpassword=$RPC_PASS \
-    -rpcport=$RPC_PORT \
+TESTNET="${TESTNET:-0}"
+DEBIAN_FRONTEND="${DEBIAN_FRONTEND:-noninteractive}"
+EXTRAFLAGS="${EXTRAFLAGS:-}"
+
+exec litecoind -rpcport=$RPC_PORT \
     -rpcallowip=$RPC_ALLOW_IP \
     -rpcbind=$RPC_BIND \
-    -listen \
+    -rpcuser=$RPC_USER \
+    -rpcpassword=$RPC_PASS \
+    -rest \
     -server \
     -testnet=$TESTNET \
     $EXTRAFLAGS
