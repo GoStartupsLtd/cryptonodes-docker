818e2310b96d2001751722b9e60a35b3c6f4af39
Dean Donkov <nezpower@abv.bg>
2023-06-26 16:38:50 +0300

    Fix PR comments

--- Dogecoin/run.sh
+++ Dogecoin/run.sh
@@ -4,12 +4,11 @@
 
 RPC_USER="${RPC_USER:-user}"
 RPC_PASS="${RPC_PASS:-pass}"
-RPC_PORT="${RPC_PORT:-22554}"
+RPC_PORT="${RPC_PORT:-22555}"
 RPC_ALLOW_IP="${RPC_ALLOW_IP:-0.0.0.0/0}"
-TESTNET="${TESTNET:-0}"
-EXTRAFLAGS="${EXTRAFLAGS:-""}"
 DEBIAN_FRONTEND="${DEBIAN_FRONTEND:-noninteractive}"
 RPC_BIND="${RPC_BIND:-0.0.0.0}"
+TESTNET="${TESTNET:-0}"
 EXTRAFLAGS="${EXTRAFLAGS:-}"
 
 exec dogecoind -rest -server -printtoconsole \
