967dffa278177daf1c31da1f9d3418ed066268fb
Dean Donkov <nezpower@abv.bg>
2023-07-08 18:10:59 +0300

    Update bitcoin-like nodes (#29)
    * Update bitcoin-like nodes
    
    * Fix PR comments
    
    * Fix permissions

--- Bitcoin/run.sh
+++ Bitcoin/run.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
+# If no RPC password is set the node will use a randomly generated password located at ~/root/.bitcoin/.cookie
+
 RPC_USER="${RPC_USER:-user}"
 RPC_PASS="${RPC_PASS:-pass}"
 RPC_PORT="${RPC_PORT:-8332}"
-RPC_ALLOW_IP="${RPC_ALLOW_IP:-127.0.0.1}"
-ZMQ_PORT="${ZMQ_PORT:-29000}"
-ZMQ_ADDRESS="${ZMQ_ADDRESS:-localhost}"
-TESTNET="${TESTNET:-0}"
-EXTRAFLAGS="${EXTRAFLAGS:-""}"
+RPC_ALLOW_IP="${RPC_ALLOW_IP:-0.0.0.0/0}"
 DEBIAN_FRONTEND="${DEBIAN_FRONTEND:-noninteractive}"
 RPC_BIND="${RPC_BIND:-0.0.0.0}"
+TESTNET="${TESTNET:-0}"
+EXTRAFLAGS="${EXTRAFLAGS:-}"
 
-exec bitcoind -rpcuser="$RPC_USER" \
-    -rpcpassword="$RPC_PASS" \
-    -port="$RPC_PORT" \
-    -rpcallowip="$RPC_ALLOW_IP" \
-    -rpcbind="$RPC_BIND" \
-    -listen \
-    -server \
-    -zmqpubhashblock="tcp://$ZMQ_ADDRESS:$ZMQ_PORT" \
-    -zmqpubhashtx="tcp://$ZMQ_ADDRESS:$ZMQ_PORT" \
-    -zmqpubrawblock="tcp://$ZMQ_ADDRESS:$ZMQ_PORT" \
-    -zmqpubrawtx="tcp://$ZMQ_ADDRESS:$ZMQ_PORT" \
-    -zmqpubsequence="tcp://$ZMQ_ADDRESS:$ZMQ_PORT" \
+exec bitcoind -rest -server \
+    -rpcbind=$RPC_BIND \
+    -rpcallowip=$RPC_ALLOW_IP \
+    -rpcport=$RPC_PORT \
+    -rpcuser=$RPC_USER \
+    -rpcpassword=$RPC_PASS \
     -testnet=$TESTNET \
     $EXTRAFLAGS
--- Dogecoin/run.sh
+++ Dogecoin/run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-RPC_USER="${RPC_USER:-}"
-RPC_PASS="${RPC_PASS:-}"
-RPC_PORT="${RPC_PORT:-22554}"
-RPC_ALLOW_IP="${RPC_ALLOW_IP:-127.0.0.1}"
-RPC_BIND="${RPC_BIND:-0.0.0.0}"
-TESTNET="${RPC_TESTNET:-0}"
-EXTRAFLAGS="${EXTRAFLAGS:-}"
-DEBIAN_FRONTEND="${DEBIAN_FRONTEND:-noninteractive}"
+# If no RPC password is set the node will use a randomly generated password located at ~/root/.bitcoin/.cookie
 
-exec dogecoind -port=$RPC_PORT \
+RPC_USER="${RPC_USER:-user}"
+RPC_PASS="${RPC_PASS:-pass}"
+RPC_PORT="${RPC_PORT:-22555}"
+RPC_ALLOW_IP="${RPC_ALLOW_IP:-0.0.0.0/0}"
+DEBIAN_FRONTEND="${DEBIAN_FRONTEND:-noninteractive}"
+RPC_BIND="${RPC_BIND:-0.0.0.0}"
+TESTNET="${TESTNET:-0}"
+EXTRAFLAGS="${EXTRAFLAGS:-}"
+
+exec dogecoind -rest -server -printtoconsole \
+    -rpcbind=$RPC_BIND \
+    -rpcallowip=$RPC_ALLOW_IP \
+    -rpcport=$RPC_PORT \
     -rpcuser=$RPC_USER \
     -rpcpassword=$RPC_PASS \
-    -rpcallowip=$RPC_ALLOW_IP \
-    -rpcbind=$RPC_BIND \
-    -listen \
-    -server \
     -testnet=$TESTNET \
-    -printtoconsole \
     $EXTRAFLAGS
