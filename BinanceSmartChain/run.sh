f3f112121a687a3ddd159490dede6943beb07fab
Dean Donkov <nezpower@abv.bg>
2023-07-08 18:10:42 +0300

    Update ethereum-like nodes (#30)
    * Update ethereum nodes
    
    * Remove double comment
    
    * Fix PR comments
    
    * Add permissions

--- BinanceSmartChain/run.sh
+++ BinanceSmartChain/run.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-HTTP_PORT="${HTTP_PORT:-18545}"
-WS_PORT="${WS_PORT:-18546}"
-ADDRESS="${ADDRESS:-127.0.0.1}"
-PORT="${PORT:-30311}"
+awk '/\[Node.LogConfig\]/ {exit} {sub(/MaxPeers = 30/, "MaxPeers = 100");print}' /home/config.toml > /home/temp.toml && mv /home/temp.toml /home/config.toml
+
+HTTP_PORT="${HTTP_PORT:-8545}"
+WS_PORT="${WS_PORT:-8547}"
+ADDRESS="${ADDRESS:-0.0.0.0}"
+PORT="${PORT:-30303}"
 SYNCMODE="${SYNCMODE:-snap}"
 EXTRAFLAGS="${EXTRAFLAGS:-""}"
 
-exec geth_linux \
-    --ipcdisable \
-    --syncmode ${SYNCMODE} \
+exec geth --syncmode ${SYNCMODE} \
     --nat none \
-    --rpc.allow-unprotected-txs \
     --txlookuplimit 0 \
     --cache 4096 \
     --port ${PORT} \
     --http \
     --http.addr ${ADDRESS} \
+    --ipcdisable \
     --http.port=${HTTP_PORT} \
     --http.vhosts '*' \
     --http.corsdomain '*' \
--- Kucoin/run.sh
+++ Kucoin/run.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-HTTP_PORT="${HTTP_PORT:-8547}"
-WS_PORT="${WS_PORT:-8548}"
+HTTP_PORT="${HTTP_PORT:-8545}"
+WS_PORT="${WS_PORT:-8546}"
 PORT="${PORT:-30303}"
 SYNCMODE="${SYNCMODE:-snap}"
-ADDRESS="${ADDRESS:-127.0.0.1}"
+ADDRESS="${ADDRESS:-0.0.0.0}"
 EXTRAFLAGS="${EXTRAFLAGS}"
 
 exec geth --syncmode ${SYNCMODE} \
     --nat none \
+    --ipcdisable \
     --txlookuplimit 0 \
     --cache 4096 \
     --port ${PORT} \
     --http \
     --http.addr ${ADDRESS} \
-    --http.port=${HTTP_PORT} \
-    --http.vhosts '*' \
-    --http.corsdomain '*' \
+    --http.port ${HTTP_PORT} \
+    --http.vhosts "*" \
+    --http.corsdomain "*" \
     --ws \
     --ws.addr ${ADDRESS} \
     --ws.port ${WS_PORT} \
-    --ws.origins '*' \
+    --ws.origins "*" \
     ${EXTRAFLAGS}
+
+ 
